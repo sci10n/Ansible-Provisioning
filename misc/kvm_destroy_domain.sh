caaf4d7cf697ef12d59a337baabfc23ff62127a8
sciion <sciion@odin.sciion.me>
2017-02-11 20:40:29 +0100

    huge refractoring

--- misc/kvm_destroy_domain.sh
+++ misc/kvm_destroy_domain.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+set -e 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 echo "Destroying host $1..."
-virsh destroy "$1"
-virsh undefine "$1"
-rm -f "/var/kvm/$1.qcow2"
-hosts=$( cat /etc/ansible/hosts | grep -v "$1" )
-dns=$( cat /etc/bind/zones/db.sciion.me | grep -v "$1" )
-echo "$dns" > /etc/bind/zones/db.sciion.me
-echo "$hosts" > /etc/ansible/hosts
-service bind9 restart
+virsh -c qemu:///system destroy "$1"
+storage=$( virsh -c qemu:///system domstats $1 | grep -oP 'block.0.path=\K.*' )
+if [ -e "$storage" ]
+then
+	rm -f "$storage"
+else
+	echo "Storage file: $storage not found!"
+	exit 2
+fi
+virsh -c qemu:///system undefine "$1"
+echo "Destruction of host $1 successfull!"
+exit 0
